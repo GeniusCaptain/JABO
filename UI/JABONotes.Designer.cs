39ec4b8ebb361ce0292df7cedb35affe77092318
Yaroslav <alienmonsterrus@gmail.com>
2019-10-20 02:39:49 +0700

    Added todo messages saving

--- UI/JABONotes.Designer.cs
+++ UI/JABONotes.Designer.cs
@@ -28,19 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // JABONotes
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "JABONotes";
-            this.Text = "JABONotes";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.JABONotes_FormClosing);
-            this.ResumeLayout(false);
+			this.btnMessage = new System.Windows.Forms.Button();
+			this.txtTodoMessage = new System.Windows.Forms.TextBox();
+			this.lstTodoMessages = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// btnMessage
+			// 
+			this.btnMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnMessage.Location = new System.Drawing.Point(270, 392);
+			this.btnMessage.Name = "btnMessage";
+			this.btnMessage.Size = new System.Drawing.Size(150, 46);
+			this.btnMessage.TabIndex = 0;
+			this.btnMessage.Text = "Добавить";
+			this.btnMessage.UseVisualStyleBackColor = true;
+			// 
+			// txtTodoMessage
+			// 
+			this.txtTodoMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtTodoMessage.Location = new System.Drawing.Point(13, 392);
+			this.txtTodoMessage.Multiline = true;
+			this.txtTodoMessage.Name = "txtTodoMessage";
+			this.txtTodoMessage.Size = new System.Drawing.Size(251, 46);
+			this.txtTodoMessage.TabIndex = 1;
+			// 
+			// lstTodoMessages
+			// 
+			this.lstTodoMessages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lstTodoMessages.FormattingEnabled = true;
+			this.lstTodoMessages.Location = new System.Drawing.Point(13, 18);
+			this.lstTodoMessages.Name = "lstTodoMessages";
+			this.lstTodoMessages.Size = new System.Drawing.Size(407, 368);
+			this.lstTodoMessages.TabIndex = 2;
+			// 
+			// JABONotes
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(432, 450);
+			this.Controls.Add(this.lstTodoMessages);
+			this.Controls.Add(this.txtTodoMessage);
+			this.Controls.Add(this.btnMessage);
+			this.MaximumSize = new System.Drawing.Size(1024, 1024);
+			this.MinimumSize = new System.Drawing.Size(448, 489);
+			this.Name = "JABONotes";
+			this.Text = "JABONotes";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.JABONotes_FormClosing);
+			this.Load += new System.EventHandler(this.JABONotes_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.Button btnMessage;
+		private System.Windows.Forms.TextBox txtTodoMessage;
+		private System.Windows.Forms.ListBox lstTodoMessages;
+	}
 }
--- UI/JABONotes.cs
+++ UI/JABONotes.cs
@@ -1,19 +1,70 @@
-﻿using System.Windows.Forms;
+﻿using JABO.Data.Models;
+using JABO.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace JABO
 {
 	public partial class JABONotes : Form
     {
+		private List<TodoMessageModel> _todoMessages;
+		
         public JABONotes()
         {
             InitializeComponent();
+
+			// Подписываемся на событие клика кнопки.
+			btnMessage.Click += OnMessageButtonClick;
         }
 
-        private void JABONotes_FormClosing(object sender, FormClosingEventArgs e)
+		private void OnMessageButtonClick(object sender, EventArgs e)
+		{
+			var newTodoText = txtTodoMessage.Text;
+			txtTodoMessage.Text = string.Empty;
+
+			// Добавляем на в сам ListBox
+			lstTodoMessages.Items.Add(newTodoText);
+
+			// Добавляем в локальный лист 
+			// (например, что бы потом его сохранить, не терять дату и т.д)
+			_todoMessages.Add(new TodoMessageModel
+			{
+				AddedDate = DateTime.Now,
+				Message = newTodoText
+			});
+		}
+
+		// Этот метод выполнится один раз за всю жизнь приложения,
+		// т.к. у нас кешируеются формы в FormManager-е
+		private void JABONotes_Load(object sender, EventArgs e)
+		{
+			// Получаем модели из репозитория.
+			_todoMessages = TodoMessagesRepository.Load().ToList();
+
+			// Берем из наших данных только текст.
+			var messages = _todoMessages.Select(c => c.Message).ToArray();
+
+			// Суем его в наш ListBox
+			lstTodoMessages.Items.AddRange(messages);
+		}
+
+		private void JABONotes_FormClosing(object sender, FormClosingEventArgs e)
         {
+			// Когда формочку закрываем, тогда сохраняем 
+			// в репозиторий все наши данные.
+			TodoMessagesRepository.Save(_todoMessages);
+
+			// Бере нашу основную формочку и показываем ее.
             FormManager.Instance.GetForm<JABO>().Show();
+
+			// Устанавливая этот флаг мы по сути говорим,
+			// что форму закрывать не надо. 
 			e.Cancel = true;
+
+			// Вместо этого мы ее скрываем (это разные вещи).
 			Hide();
         }
-    }
+	}
 }
