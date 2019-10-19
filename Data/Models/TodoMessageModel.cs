30625efefd4da737280f5600472cd67d9c6f938c
Yaroslav <alienmonsterrus@gmail.com>
2019-10-20 02:15:07 +0700

    Added Json.Net, Added TodoMessagesRepository

---
+++ Data/Models/TodoMessageModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace JABO.Data.Models
+{
+	public class TodoMessageModel
+	{
+		public string Message { get; set; }
+		public DateTime AddedDate { get; set; }
+	}
+}
---
+++ Data/Repository/TodoMessagesRepository.cs
@@ -0,0 +1,36 @@
+﻿using JABO.Data.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace JABO.Data.Repository
+{
+	public static class TodoMessagesRepository
+	{
+		public const string FilePath = "todo.json";
+
+		public static IEnumerable<TodoMessageModel> Load()
+		{
+			try
+			{
+				var json = File.ReadAllText(FilePath);
+
+				return JsonConvert.DeserializeObject<
+					List<TodoMessageModel>>(json);
+			}
+			catch
+			{
+				return new List<TodoMessageModel>();
+			}
+		}
+
+		public static void Save(IEnumerable<TodoMessageModel> messages)
+		{
+			var json = JsonConvert.SerializeObject(
+				messages.ToList(), Formatting.Indented);
+
+			File.WriteAllText(FilePath, json);
+		}
+	}
+}
